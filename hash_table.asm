0f7e4ecd2d430d173b9fda97b255e39ac3cee3e8
Akreson <gimules.but@gmail.com>
2024-04-21 15:40:35 +0200

    Add realloc to ht add entry

--- hash_table.asm
+++ hash_table.asm
@@ -79,9 +79,8 @@ _exit_ht_find_entry:
 hash_table_add_entry:
     push rbp
     mov rbp, rsp
-    push rdi
-    push rsi
-    push rdx
+    sub rsp, 56
+    mov [rbp-8], rdi
 
     xor rax, rax
     test rdi, rdi
@@ -92,25 +91,78 @@ hash_table_add_entry:
     jz _exit_ht_add_entry
     cmp rsi, rdi
     jb _exit_ht_add_entry
-    mov rbx, rdi
+    mov rbx, [rdi]
     mov ecx, [rdi+12]
-    add rbx, rcx
-    cmp rsi, rdi
+    lea r8, [rbx+rcx*8]
+    cmp rsi, r8
     jge _exit_ht_add_entry
 
     mov [rsi], rdx
-    mov rbx, [rdi]
     mov ebx, [rdi+8]
+    inc ebx
+    mov [rdi+8], ebx
     mov eax, ecx
     mov r8d, ecx
-    shl eax, 1
-    shl r8d, 2
+    shr eax, 1
+    shr r8d, 2
     add eax, r8d
-    cmp ecx, eax
-    jb _exit_ht_add_entry
-    ;realloc all entries
+    cmp ebx, eax
+    jb _success_exit_ht_add_entry 
+    mov edi, ecx
+    shl edi, 1
+    call mmap_def
+    xor rdx, rdx
+    sub rdx, 1
+    cmp rax, rdx   
+    jne _start_realloc_ht
+    xor rax, rax
+    jmp _exit_ht_add_entry
+_start_realloc_ht:
+    mov ecx, 16
+    mov rsi, [rbp-8]
+    lea rdi, [rbp-32]
+    rep movsb
+    mov rdi, [rbp-8]
+    mov ecx, [rdi+12]
+    mov r8, [rdi]
+    lea r9, [r8+rcx*8]
+    shl ecx, 1
+    mov [rdi], rax
+    mov [rdi+12], ecx
+    mov [rbp-48], r9
+_start_realloc_loop_ht:
+    mov rax, [r8]
+    test rax, rax
+    jz _next_realloc_loop_ht
+    mov [rbp-40], r8
+    mov rdi, [rax]
+    movzx esi, byte [rax+13]
+    mov [rbp-56], rax
+    call hash_str
+    mov rbx, [rbp-56]
+    mov rdi, [rbp-8]
+    mov rsi, [rbx]
+    movzx edx, byte [rbx+13]
+    mov ecx, eax
+    call hash_table_find_entry
+    mov rbx, [rbp-56]
+    mov [rax], rbx
+    mov r8, [rbp-40]
+    mov r9, [rbp-48]
+_next_realloc_loop_ht:
+    add r8, 8
+    cmp r8, r9
+    jb _start_realloc_loop_ht
+    mov rdi, [rbp-32]
+    mov esi, [rbp-20]
+    call munmap
+    test rax, rax
+    jz _success_exit_ht_add_entry
+    exit_m -10
+_success_exit_ht_add_entry:
+    mov rax, 1
 _exit_ht_add_entry:
-    add rsp, 24
+    add rsp, 56
     pop rbp
     ret
 
