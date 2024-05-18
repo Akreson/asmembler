1f94dc0172bf1fb4069ac949251ce216e391d6d9
Akreson <gimules.but@gmail.com>
2024-05-18 17:50:45 +0200

    Add linked list for _name_ symbol

---
+++ list.asm
@@ -0,0 +1,169 @@
+segment readable executable
+; first entry at offset 0 is reserved as invalid offset
+
+;0 ptr to mem, +8 count, +12 capacity, +16 entry size,
+;+20 free next offset, +24 ?
+macro list_main_block_m
+{
+    ;entry array main block
+    dq 0
+    dd 0, 0, 0
+
+    ;sentinal of free and in use nodes
+    ;TODO: remove in use node track?
+    dd 0, 0
+}
+
+LIST_BLOCK_SIZE equ 24
+; list entry
+; 0 next offset, +4 data ...
+
+;does not modifies ebx-rsi reg
+;rdi - ptr to list main block, esi - offset of node
+;return rax - ptr to node
+list_get_node_ptr:
+    mov eax, [rdi+16]
+    mov r9d, [rdi+12]
+    mul r9d
+    cmp esi, eax
+    ja _out_of_range_list_get
+    mov r8, [rdi]
+    lea rax, [r8+rsi]
+    jmp _end_list_get_node_ptr
+_out_of_range_list_get:
+    xor rax, rax
+_end_list_get_node_ptr:
+    ret
+
+;rdi - ptr to list main block, esi - offset to insert entry, edx - offset to curr lead node
+;return eax = 0 if err, eax = passed esi
+list_insert_node:
+    push rbp
+    mov rbp, rsp
+    test esi, esi
+    jz _out_of_range_list_insert
+    mov r8d, edx
+    mov eax, [rdi+16]
+    mov ecx, [rdi+12]
+    mul ecx
+    cmp esi, eax
+    ja _out_of_range_list_insert  
+    cmp r8d, eax
+    ja _out_of_range_list_insert
+    mov rbx, [rdi]
+    mov [rbx+rsi], r8d
+    mov eax, esi
+    jmp _end_list_insert
+_out_of_range_list_insert:
+    xor eax, eax
+_end_list_insert:
+    pop rbp
+    ret
+
+;rdi - ptr to list main block, esi - offset to node to free
+;return eax - next entry offset
+list_free_node:
+    push rbp
+    mov rbp, rsp
+    sub rsp, 4
+    mov rbx, [rdi]
+    mov eax, [rbx+rsi]
+    mov [rbp-4], eax
+    mov edx, [rdi+20]
+    call list_insert_node
+    test eax, eax
+    jz _err_list_free_node
+    mov ecx, eax
+    mov [rdi+20], ecx
+    mov eax, [rbp-4]
+    jmp _end_list_free_node
+_err_list_free_node:
+    xor eax, eax
+_end_list_free_node:
+    add rsp, 4
+    pop rbp
+    ret
+
+;rdi - ptr to list main block
+;return eax - offset, rbx - ptr to block
+list_check_get_free:
+    push rbp
+    mov rbp, rsp
+    sub rsp, 8
+    mov [rbp-8], rdi
+    mov esi, 1
+    call entry_array_check_get
+    test rax, rax
+    jz _check_free_list_lcgf 
+    mov rbx, rax
+    mov rdi, [rbp-8]
+    mov rdx, [rdi]
+    sub rax, rdx
+    jmp _end_dlist_check_get_entry 
+_check_free_list_lcgf:
+    mov rdi, [rbp-8]
+    mov ebx, [rdi+20]
+    test ebx, ebx
+    jz _check_fail_dcgf
+    mov rdx, [rdi]
+    lea r8, [rdx+rbx]
+    mov ecx, [r8]
+    mov [rdi+20], ecx
+    mov eax, ebx
+    mov rbx, r8
+    jmp _end_dlist_check_get_entry
+_check_fail_dcgf:
+    xor rax, rax
+_end_dlist_check_get_entry:
+    add rsp, 8
+    pop rbp
+    ret
+
+;rdi - ptr to list main block, esi - new capacity
+list_realloc:
+    push rbp
+    mov rbp, rsp
+    sub rsp, 40
+    mov [rbp-40], rdi
+    mov rbx, [rdi]
+    mov [rbp-32], rbx
+    lea rdx, [rbp-24]
+    call entry_array_copy_realloc
+    test rax, rax
+    jnz _success_list_realloc
+    exit_m -9
+_success_list_realloc:
+    mov rdi, [rbp-32]
+    call entry_array_dealloc
+    mov rdi, [rbp-40]
+    lea rsi, [rbp-24]
+    mov ecx, LIST_BLOCK_SIZE
+    rep movsb
+_end_list_realloc:
+    add rsp, 40
+    pop rbp
+    ret
+
+list_dealloc:
+;TODO: implement
+
+;rdi - ptr to dlist main block, rsi - started count of entries
+init_list:
+    push rbp
+    mov rbp, rsp
+    sub rsp, 8
+    mov [rbp-8], rdi
+    inc rsi
+    call init_entry_array
+    test rax, rax
+    jz _fail_init_dlist
+    mov rdi, [rbp-8]
+    mov dword [rdi+8], 1
+    mov eax, 1
+    jmp _end_init_dlist
+_fail_init_dlist:
+    xor rax, rax
+_end_init_dlist:
+    add rsp, 8
+    pop rbp
+    ret
--- parser.asm
+++ parser.asm
@@ -3,12 +3,14 @@
 segment readable writeable
 
 ; entry
-; 0 ptr to linked list of location to patch, +4 id in segment arr, +4 id in file array
+; 0 (4b) offset to linked list of location to patch, 
 ; +16 symbol entry (round up to multible of 8, curr 16) (32b total)
 UNK_ENTRY_SIZE equ 32
+; linked list body - +4 id in segment arr, +4 offset to patch
 ; ptr, count in entries, capacity in entries, entry size
 UNKNOWN_NAME_SYM_REF_ARRAY dq 0
 dd 0, 0, UNK_ENTRY_SIZE
+
 
 ; entry
 ; 0 data size, +4 id in segment arr, +4 offset in file, +4 line num in file
@@ -27,16 +29,15 @@ dd 0, 0
 
 
 ;entry array (for ease mem mang.) + 
-PATCH_DLIST dq 0
+PATCH_LIST dq 0
 dd 0, 0, 0
-dd 0, 0
 dd 0, 0
 
 ; token buff
 ; (header)
 ; 0(4) offset in render buff, +4(4) line num, +8(2) offset on line, +10(2) entry size in byte
 ; (body) 
-; +12(1) token type, +13 [(8) ptr ot token | (TOKEN_KIND_SIZE) token body] ... (n times)
+; +12(1) token type, +13 [(8) ptr to token | (TOKEN_KIND_SIZE) token body] ... (n times)
 
 segment readable executable
 
@@ -118,13 +119,91 @@ _add_entry_pnt_unk:
     mov rdx, rax
     call hash_table_add_entry
     test rax, rax
-    mov rax, [rbp-48]
-    ;TODO: alloc dlist entry
+    ;TODO: init header
+    ;TODO: add check
+    mov rdi, PATCH_LIST
+    call list_check_get_free
+    test eax, eax
+    jnz _node_fetch_succ_pnt_unk
+    mov rdi, PATCH_LIST
+    mov esi, [rdi+12]
+    shl esi, 1
+    call list_realloc
+    mov rdi, PATCH_LIST
+    call list_check_get_free
+_node_fetch_succ_pnt_unk:
+    mov r8, [rbp-48]
+    mov [r8], eax 
+    ;TODO: init list entry
 _end_push_name_to_unk:
     add rsp, 68
     pop rbp
     ret
 
+;rdi - ptr to symbol entry, esi - segment id, edx - offset to patch
+push_link_to_unk:
+    push rbp
+    mov rbp, rsp
+    sub rsp, 16
+    sub rdi, 16
+    mov [rbp-8], rdi
+    mov [rbp-12], esi
+    mov [rbp-16], edx
+    mov rdi, PATCH_LIST
+    call list_check_get_free
+    test eax, eax
+    jnz _add_link_to_chain_unk 
+    mov rdi, PATCH_LIST
+    mov esi, [rdi+12]
+    shl esi, 1
+    call list_realloc
+    mov rdi, PATCH_LIST
+    call list_check_get_free
+_add_link_to_chain_unk:
+    mov esi, eax
+    mov ecx, [rbp-12]
+    mov eax, [rbp-16]
+    mov [rbx+4], ecx
+    mov [rbx+8], eax
+    mov rax, [rbp-8]
+    mov edx, [rax]
+    mov rdi, PATCH_LIST
+    call list_insert_node
+    test eax, eax
+    jnz _finish_add_link_unk
+    exit_m -7
+_finish_add_link_unk:
+    mov rbx, [rbp-8]
+    mov [rbx], eax
+_end_push_link_to_unk:
+    add rsp, 16
+    pop rbp
+    ret
+
+;TODO: complete
+;rdi - ptr to unk symbol
+patch_unk_ref:
+    push rbp
+    mov rbp, rsp
+    sub rsp, 16
+    mov [rbp-8], rdi
+    mov rbx, rdi
+    sub rbx, 16
+_loop_patch_ur:
+    mov rdi, PATCH_LIST
+    mov esi, [rbx]
+    call list_free_node
+    test eax, eax
+    jz _end_patch_unk_ref
+    mov rbx, [rbp-8]
+    mov [rbx], eax
+    jmp _loop_patch_ur
+_end_patch_unk_ref:
+    add rsp, 16
+    pop rbp
+    ret
+
+;-16 token 0, -32 token 1, -40 passed rdi, -48 ptr to token 
 ; rdi - ptr to file entry
 start_parser:
     push rbp
@@ -138,8 +217,8 @@ _new_entry_start_ps:
     test rax, rax
     jz _end_start_parser
     movzx eax, byte [rbp-4]
-    cmp eax, TOKEN_TYPE_INS
-    je _begin_ins_sp
+    ;cmp eax, TOKEN_TYPE_INS
+    ;je _begin_ins_sp
     cmp eax, TOKEN_TYPE_NAME
     je _begin_name_sp
     cmp eax, TOKEN_TYPE_EOF
@@ -153,10 +232,36 @@ _begin_name_sp:
     call hash_table_find_entry
     mov rbx, [rax]
     test rbx, rbx
-    jnz _next_test_sp
+    jnz _check_next_sym
     lea rsi, [rbp-16]
     mov rdi, rax
     call push_name_to_unk
+    jmp _print_buff_info
+_check_next_sym:
+    mov [rbp-48], rbx
+    mov rdi, [rbp-40]
+    lea rsi, [rbp-32]
+    call next_token
+    test rax, rax
+    jz _end_start_parser
+    movzx eax, byte [rbp-20]
+    cmp eax, TOKEN_TYPE_EOF
+    je _add_to_chain
+    cmp eax, TOKEN_TYPE_AUX
+    jne _new_entry_start_ps
+    mov eax, [rbp-24]
+    cmp eax, AUX_COLON
+    jne _add_to_chain
+    mov rdi, [rbp-48]
+    call patch_unk_ref
+    jmp _new_entry_start_ps
+_add_to_chain:
+    mov rdi, [rbp-48]
+    mov esi, 0
+    mov edx, 0
+    call push_link_to_unk
+    jmp _new_entry_start_ps
+_print_buff_info:
     movzx rdi, byte [rbp-4]
     mov rsi, 10
     call print_u_digit
@@ -194,7 +299,7 @@ init_parser_data:
     test rax, rax
     jz _fail_exit_init_parser_data 
     mov rdi, UNKNOWN_NAME_SYM_REF_ARRAY
-    mov rsi, 1; TODO: change to 200
+    mov rsi, 1; TODO: change to 256
     call init_entry_array
     test rax, rax
     jz _fail_exit_init_parser_data
@@ -209,8 +314,12 @@ init_parser_data:
     call init_entry_array
     test rax, rax
     jz _fail_exit_init_parser_data
-    
-    ;TODO: init DLIST
+    mov rdi, PATCH_LIST
+    mov dword [rdi+16], 16
+    mov esi, 5;TODO: change to 256
+    call init_list
+    test rax, rax
+    jz _fail_exit_init_parser_data
     mov rax, 1
     jmp _end_init_parser_data
 _fail_exit_init_parser_data:
