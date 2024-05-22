ffa8989ea8d92f7af974b5f89c841e0530d4ebf0
Akreson <gimules.but@gmail.com>
2024-05-22 19:17:54 +0200

    Fix typo

--- parser.asm
+++ parser.asm
@@ -13,13 +13,13 @@ dd 0, 0, UNK_ENTRY_SIZE
 
 
 ; entry
-; 0 data size, +4 id in segment arr, +4 offset in file, +4 line num in file
+; 0 data size, +4 id in segment arr, +8 offset in file, +12 line num in file
 ; +16 symbol entry (roud up), (header size 32b)
 ; +32 start of data in _token buff_ format
 NAME_SYM_REF_ARRAY dq 0
 dd 0, 0, 1
 
-; TODO: complite
+; TODO: complete
 ; entry - 0 ptr to tokens byff, +8 file array id, +12 mod (4b) (16b total)
 SEG_ENTRY_ARRAY dq 0
 dd 0, 0, 16
