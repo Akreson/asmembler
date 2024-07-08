6dacfb2c82e52af9d5842e07fbc77842afc7ceac
Akreson <gimules.but@gmail.com>
2024-07-08 17:52:57 +0200

    Continue setting up basic cheks

--- render.asm
+++ render.asm
@@ -1,3 +1,6 @@
+
+
+
 segment readable executable
 
 ; rdi - ptr to buf of ptr to seg entry
@@ -39,7 +42,13 @@ render_err_first_param:
     pop rbp
     ret
 
-; -8 passed rdi, - 16 passed rsi
+; rdi - ptr to ins param, rsi
+process_gen_r_r:
+    push rbp
+    mov rbp, rsp
+    pop rbp
+    ret
+; -8 passed rdi, - 16 passed rsi, -24 curr ins build ptr
 ; rdi - segment ptr, rsi - ptr to token entry to process
 process_mov:
     push rbp
@@ -47,28 +56,80 @@ process_mov:
     sub rsp, 128
     mov [rbp-8], rdi
     mov [rbp-16], rsi
+    mov [rbp-24], rsp
     mov eax, [rdi+28]
     mov [rsi], eax
     add rsi, TOKEN_HEADER_PLUS_DIRECT 
     movzx ebx, byte [rsi]
     cmp ebx, TOKEN_BUF_DIRECT
-    je _mov_f_r
+    je _mov_r
     cmp ebx, TOKEN_BUF_ADDR
-    je _mov_f_a
-    jmp _err_invalid_first_param
-_mov_f_r:
+    je _mov_a
+    jmp _err_invalid_first_param_mov
+_mov_r:
     movzx eax, byte [rsi+14]
-    cmp eax, TOKEN_TYPE_REG
-    jne _err_invalid_first_param
-    mov ebx, [rsi+9]
-    mov edx, ebx
+    mov esi, [rsi+9]
+    mov edx, esi
+    and esi, REG_MASK_REG_VAL
     and edx, REG_MASK_BITS
-    ;TODO: complete 
-_mov_f_a:
-_err_invalid_first_param:
+    cmp eax, TOKEN_TYPE_REG
+    jne _err_invalid_first_param_mov
+    lea r9, [rsi+15]
+    movzx ecx, byte [r9]
+    cmp ecx, TOKEN_BUF_ADDR
+    je __mov_r_a
+    cmp ecx, TOKEN_BUF_PTR_OFFSET
+    je __mov_r_i
+    cmp ecx, TOKEN_BUF_DIRECT
+    jne _err_invalid_second_param_mov
+    movzx ebx, byte [r9+14]
+    cmp ebx, TOKEN_TYPE_REG
+    je __mov_r_r
+    cmp ebx, TOKEN_TYPE_DIGIT
+    je __mov_r_i
+    jmp _err_invalid_second_param_mov
+__mov_r_r:
+    cmp edx, REG_MASK_VAL_64B
+    je __mov_r_r64
+    cmp edx, REG_MASK_VAL_32B
+    je __mov_r_r32
+    cmp edx, REG_MASK_VAL_16B
+    je __mov_r_r16
+    cmp edx, REG_MASK_VAL_8B
+    jne _err_invalid_first_param_mov
+___mov_r_r16:
+___mov_r_r32:
+___mov_r_r64:
+__mov_r_a:
+    cmp edx, REG_MASK_VAL_64B
+    je __mov_r_a64
+    cmp edx, REG_MASK_VAL_32B
+    je __mov_r_a32
+    cmp edx, REG_MASK_VAL_16B
+    je __mov_r_a16
+    cmp edx, REG_MASK_VAL_8B
+    jne _err_invalid_first_param_mov
+___mov_r_a16:
+___mov_r_a32:
+___mov_r_a64:
+__mov_r_i:
+    cmp edx, REG_MASK_VAL_64B
+    je __mov_r_r64
+    cmp edx, REG_MASK_VAL_32B
+    je __mov_r_r32
+    cmp edx, REG_MASK_VAL_16B
+    je __mov_r_r16
+    cmp edx, REG_MASK_VAL_8B
+    jne _err_invalid_first_param_mov
+___mov_r_i16:
+___mov_r_i32:
+___mov_r_i64:
+_mov_a:
+_err_invalid_second_param_mov:
+_err_invalid_first_param_mov:
     mov rdi, [rbp-16]
     call render_err_first_param
-_err_exit:
+_err_exit_mov:
     exit_m -6
 _end_process_mov:
     add rsp, 128
