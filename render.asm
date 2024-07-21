b36959c39c42bf72064ed257c49a31216a1f3972
Akreson <gimules.but@gmail.com>
2024-07-21 14:42:31 +0200

    Fix mov r_imm ins render

--- render.asm
+++ render.asm
@@ -1,9 +1,11 @@
 
 
 ; ins code struct (32 bytes)
-; 0 (16 bytes) post opcodes bytes, +16 (8 bytes) prefix bytes, +24 len post opcode,
-; +25 prefix bytes, +26 1st arg size, +27 2nd arg size, +28 3rd arg size (3 bytes reserved)
+; 0 (16 bytes) post opcodes bytes (1st byte is ModR/M), +16 (8 bytes) prefix bytes, +24 post opcode bytes count,
+; +25 prefix bytes count, +26 1st arg size, +27 2nd arg size, +28 3rd arg size (3 bytes reserved)
 INS_CODE_STRUCT_SIZE equ 32
+
+MOD_RM_RM_MASK equ 0x07
 
 REX   equ 0x40
 REX_W equ 0x08
@@ -92,6 +94,9 @@ render_err_first_param:
     pop rbp
     ret
 
+; rdi - dest, rsi - source, ecx - size
+shift_and_clear_postfix_buf:
+
 ; for r_r version by default used r, r/m version
 ; rdi - ptr to ins param, rsi - ptr to ins code struct 
 ; return eax - 0 if succes 
@@ -152,7 +157,7 @@ _gen_r_r_set_prefix:
     movzx eax, byte [rsi+25]
     mov byte [r11+rax], r9b
     inc eax
-    mov byte [rsi+25], al
+    mov [rsi+25], al
     jmp _success_gen_r_r
 _err_gen_r_r_unrec_size:
 _err_gen_r_r_unmatch_size:
@@ -161,6 +166,108 @@ _err_gen_r_r_unmatch_size:
 _success_gen_r_r:
     xor eax, eax
 _end_process_gen_r_r:
+    add rsp, 16
+    pop rbp
+    ret
+
+; for r_i version by default used r/m, imm version
+; rdi - ptr to ins param, rsi - ptr to inc code struct
+; return eax - 0 if succes, 1 if imm less then reg
+process_gen_rm_i:
+    push rbp
+    mov rbp, rsp
+    sub rsp, 16
+    mov [rbp-16], rsi
+    mov r10, rsi
+    xor r9, r9
+    lea r8, [rdi+15]
+    mov eax, [rdi+9]
+    mov ebx, [r8+14]
+    mov edx, eax
+    and eax, REG_MASK_REG_VAL
+    and edx, REG_MASK_BITS
+    mov [rsi+26], edx
+    cmp edx, REG_MASK_VAL_64B
+    jne _gen_rm_i_check_arg_th
+    or r9b, REX_W
+_gen_rm_i_check_arg_th:
+    cmp eax, REG_REX_TH
+    jb _gen_rm_i_set_rex
+    or r9b, REX_B
+    and eax, REG_MASK_REG_IDX
+_gen_rm_i_set_rex:
+    test r9b, r9b
+    jz _gen_rm_i_set_arg
+    or r9b, REX
+_gen_rm_i_set_arg:
+    xor r12, r12
+    lea r11, [r10+16]
+    or r12b, MOD_REG
+    or r12b, al
+    mov byte [r10], r12b
+    inc byte [rsi+24]
+    cmp edx, REG_MASK_VAL_16B
+    jne _gen_rm_i_set_prefix
+    mov byte [r11], PREFIX_16BIT
+    inc byte [rsi+25]
+_gen_rm_i_set_prefix:
+    test r9b, r9b
+    jz _gen_rm_i_set_postfix
+    movzx eax, byte [rsi+25]
+    mov byte [r11+rax], r9b
+    inc eax
+    mov byte [rsi+25], al
+_gen_rm_i_set_postfix:
+    movzx eax, byte [rsi+24]
+    cmp ebx, 8
+    ja __gen_rm_i_check_imm64
+    mov byte [rsi+27], REG_MASK_VAL_8B
+    mov cl, [r8+1]
+    mov [rsi+rax], cl
+    inc eax
+    mov [rsi+24], al
+    jmp _success_gen_rm_i
+__gen_rm_i_check_imm64:
+    cmp edx, REG_MASK_VAL_64B
+    jne __gen_rm_i_check_imm32
+    cmp ebx, 33
+    jb ___gen_rm_i_check_imm64_32
+    mov byte [rsi+27], REG_MASK_VAL_64B
+    mov rcx, [r8+1]
+    mov [rsi+rax], rcx
+    add eax, 8
+    mov [rsi+24], al
+    jmp _success_gen_rm_i
+___gen_rm_i_check_imm64_32:
+    mov byte [rsi+27], REG_MASK_VAL_32B
+    mov ecx, [r8+1]
+    mov [rsi+rax], ecx
+    add eax, 4
+    mov [rsi+24], al
+    jmp _success_gen_rm_i
+__gen_rm_i_check_imm32:
+    cmp edx, REG_MASK_VAL_32B
+    jne __gen_rm_i_check_imm16
+    cmp ebx, 32
+    ja _err_gen_rm_i_imm_overflow
+    jmp ___gen_rm_i_check_imm64_32
+__gen_rm_i_check_imm16:
+    cmp edx, REG_MASK_VAL_16B
+    jne _err_gen_rm_i_imm_overflow
+    cmp ebx, 16
+    ja _err_gen_rm_i_imm_overflow
+    mov byte [rsi+27], REG_MASK_VAL_16B
+    mov cx, [r8+1]
+    mov [rsi+rax], cx
+    add eax, 2
+    mov [rsi+24], al
+    jmp _success_gen_rm_i
+_err_gen_rm_i_imm_overflow:
+    mov eax, 1
+    jmp _end_process_gen_rm_i
+_success_gen_rm_i:
+    xor eax, eax
+_end_process_gen_rm_i:
     add rsp, 16
     pop rbp
     ret
@@ -227,6 +334,73 @@ ___mov_r_r_non_byte_opcode:
     jmp _mov_accemble
 __mov_r_a:
 __mov_r_i:
+    mov rdi, rsi
+    lea rsi, [rbp-128]
+    call process_gen_rm_i
+    test eax, eax
+    jnz _err_parse_mov
+    lea r8, [rbp-128]
+    movzx ebx, byte [r8+26]
+    cmp ebx, REG_MASK_VAL_8B
+    jne ___mov_r_i_non_byte_opcode
+    mov byte [rbp-42], 0xB0
+    jmp ___mov_r_i_reg_opc_ext 
+___mov_r_i_non_byte_opcode:
+    mov byte [rbp-42], 0xB8
+    cmp ebx, REG_MASK_VAL_64B
+    jne ___mov_r_i_reg_opc_ext
+    movzx eax, byte [r8+27]
+    cmp eax, REG_MASK_VAL_32B
+    ja ___mov_r_i_reg_opc_ext
+    mov byte [rbp-42], 0xC7
+    cmp eax, REG_MASK_VAL_32B
+    je _mov_accemble
+    mov r10d, 3
+    mov r11d, 2
+    xor eax, eax
+    cmp eax, REG_MASK_VAL_8B
+    cmove ecx, r10d
+    cmp eax, REG_MASK_VAL_16B
+    cmove ecx, r11d
+    movzx edx, byte [r8+24]
+    lea r9, [r8+rdx]
+    jmp ___mov_r_i_reg_opc_ext_mod 
+___mov_r_i_reg_opc_ext:
+    movzx ebx, byte [rbp-42] 
+    movzx eax, byte [r8]
+    and eax, MOD_RM_RM_MASK
+    or ebx, eax
+    mov [rbp-42], bl
+    movzx ecx, byte [r8+24]
+    dec ecx
+    mov byte [r8+24], cl
+    mov r9, r8
+    mov rdi, r8
+    inc r9
+    mov rsi, r9
+    rep movsb
+    movzx eax, byte [r8+27]
+    movzx ebx, byte [r8+26]
+    cmp eax, REG_MASK_VAL_8B
+    jne _mov_accemble
+    cmp ebx, REG_MASK_VAL_8B
+    je _mov_accemble
+    mov r10d, 1
+    mov r11d, 3
+    mov r12d, 7
+    xor eax, eax
+    cmp ebx, REG_MASK_VAL_16B
+    cmove ecx, r10d
+    cmp ebx, REG_MASK_VAL_32B
+    cmove ecx, r11d
+    cmp ebx, REG_MASK_VAL_64B
+    cmove ecx, r12d
+___mov_r_i_reg_opc_ext_mod:
+    mov r13d, ecx
+    lea rdi, [r9]
+    rep stosb
+    add [r8+24], r13b
+    jmp _mov_accemble
 _mov_a:
 __mov_a_r:
 __mov_a_i:
