3996e58fe89eb92e2e12987e316a65890fd42203
Akreson <gimules.but@gmail.com>
2024-10-10 18:33:59 +0200

    Use entry_arrat struct for FILES_ARRAY

--- files.asm
+++ files.asm
@@ -4,11 +4,12 @@ segment readable writeable
 
 ;file array
 ;entry format - 0 ptr to file data, +8 alloc data size, +16 read pos, +24 ptr to str name,
-;+32 inode, +40 name len (20 bytes reserved) 
+;+32 inode, +40 name len, +44 curr line num, +48 offset to parent entry,
+;+52 offset in parent buff, +56 line in parent buff (16 bytes reserved) 
 FILE_ARRAY_ENTRY_SIZE equ 64
 ;array - 0 ptr to buf, +8 count, +12 capacity
 FILES_ARRAY dq 0
-dd 0, 0
+dd 0, 0, 64
 
 segment readable executable
 
@@ -36,55 +37,6 @@ _fail_get_fobp:
     xor eax, eax
     dec eax
 _end_get_file_offset_by_ptr:
-    pop rbp
-    ret
-
-get_free_file_entry:
-    push rbp
-    mov rbp, rsp
-    sub rsp, 24
-    mov rdi, FILES_ARRAY
-    mov rdx, [rdi]
-    mov ecx, [rdi+8]
-    mov ebx, [rdi+12]
-    cmp ecx, ebx
-    jne _fetch_entry_ptr_gffe
-    mov [rbp-16], rdi
-    mov edi, ebx
-    shl edi, 1
-    mov [rbp-4], edi
-    shl edi, 6
-    call mmap_def
-    xor r8, r8
-    cmp rax, r8
-    jl _exit_get_free_file_entry
-    mov [rbp-24], rax
-    mov rbx, [rbp-16]
-    mov rsi, [rbx]
-    mov ecx, [rbx+8]
-    shl ecx, 6
-    mov rdi, rax
-    rep movsb
-    mov rdi, [rbx]
-    mov esi, [rbx+12]
-    shl esi, 6
-    call munmap
-    test rax, rax
-    jnz _exit_get_free_file_entry
-    mov rdi, [rbp-16]
-    mov rdx, [rbp-24]
-    mov esi, [rbp-4]
-    mov [rdi], rdx
-    mov [rdi+12], esi
-    mov ecx, [rdi+8]
-_fetch_entry_ptr_gffe:
-    mov ebx, ecx
-    shl ebx, 6
-    lea rax, [rdx+rbx]
-    inc ecx
-    mov [rdi+8], ecx
-_exit_get_free_file_entry:
-    add rsp, 24
     pop rbp
     ret
 
@@ -116,7 +68,6 @@ _exit_check_if_inode_exist:
 ;rdi - ptr to path str, esi - str len
 ;-144 stat struct, -152 (4) (4 padding) str len, -160(8) str ptr, -168(8) file entry ptr,
 ;-176(8) temp fd, -184(8) temp mem, -188(4) offset of file entry, 4KiB temp path
-
 load_file_by_path:
     push rbp
     mov rbp, rsp
@@ -175,7 +126,9 @@ _check_is_file_exit_lfbp:
     call print_zero_str
     jmp _error_exit_lfbp 
 _get_file_entry_lfbp:
-    call get_free_file_entry
+    mov rdi, FILES_ARRAY
+    mov esi, 1
+    call entry_array_reserve_size
     mov [rbp-168], rax
     mov [rbp-188], ebx
     mov rdi, rsp
@@ -230,6 +183,7 @@ _save_read_file_lfbp:
     mov [rax+40], esi
     xor r8, r8
     mov [rax+16], r8
+    mov dword [rax+44], 1
     mov ebx, [rbp-188]
     jmp _exit_load_file_by_path
 _error_exit_lfbp:
@@ -247,21 +201,13 @@ _end_lfbp:
 init_file_array:
     push rbp
     mov rbp, rsp
-    mov rdi, 64
-    shl rdi, 6
-    call mmap_def
-    xor rdx, rdx
-    sub rdx, 1
-    cmp rdx, rax
-    jne _final_init_file_array
-    xor rax, rax
-    jmp _exit_init_file_array
-_final_init_file_array:
     mov rdi, FILES_ARRAY
-    mov [rdi], rax
-    mov dword [rdi+8], 0
-    mov dword [rdi+12], 64
-    mov rax, 1
+    mov rsi, 64
+    call init_entry_array
+    test rax, rax
+    jz _exit_init_file_array
+    mov rdi, FILES_ARRAY
+    mov dword [rdi+8], 1
 _exit_init_file_array:
     pop rbp
     ret
