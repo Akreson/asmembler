55e9811c9bfa870a1013cb7ee4461d4c2edffcc4
Akreson <gimules.but@gmail.com>
2024-10-12 17:22:16 +0200

    Fix ESL err

--- err_msg.asm
+++ err_msg.asm
@@ -33,23 +33,24 @@ ERR_SEGMENT_NOT_SET db "No segment have been set", 0
 ERR_STATIC_DIGIT_OVERFLOW db "Digit overlfow on data definition", 0
 ERR_INV_CONST_DEF db "Invalid constant definition", 0
 ERR_DEF_SYM db "Symbol already have been defined", 0
-ERR_INV_ADDR db "Invalid addres expresion", 0
+ERR_INV_ADDR db "Invalid address expresion", 0
 
 ; RENDER
 ERR_INS_UNSUPPORT db "Instruction is unsupported"
+ERR_EXP_UNSUPPORT db "Unsupported expression", 0
 ERR_INS_FORMAT db "Invalid instruction format", 0
 ERR_IMM_OVERFLOW db "imm value to big for selected dest", 0
 ERR_IMM_NAME_REF db "Unsupported name ref for imm value", 0
 
-ERR_INV_ADDR_SCALE db "Invalid scale index in addres ref", 0
-ERR_INV_2ND_ADDR_PARAM db "Invalid 2nd parameter in addres ref", 0
-ERR_INV_2ND_ADDR_SUB db "2nd addres parameter can't be subtracted", 0
-ERR_ADDR_REG_SIZE db "Unsopported reg size in addres parameters", 0
-ERR_INV_1ST_ADDR_PARAM db "Invalid 1st parameter in addres ref", 0
-ERR_INV_NAME_REF_ADDR db "Invalid name ref addres field", 0
-ERR_INV_ADDR_LAST_NAME db "Invalid last name ref in addres field", 0
-ERR_ADDR_SIZE_QUAL db "Size qualifier must be spesicied for addres ref", 0
-ERR_INV_ADDR_ARGC db "Invalid amount of parameters for addres field", 0
+ERR_INV_ADDR_SCALE db "Invalid scale index in address ref", 0
+ERR_INV_2ND_ADDR_PARAM db "Invalid 2nd parameter in address ref", 0
+ERR_INV_2ND_ADDR_SUB db "2nd address parameter can't be subtracted", 0
+ERR_ADDR_REG_SIZE db "Unsopported reg size in address parameters", 0
+ERR_INV_1ST_ADDR_PARAM db "Invalid 1st parameter in address ref", 0
+ERR_INV_NAME_REF_ADDR db "Invalid name ref address field", 0
+ERR_INV_ADDR_LAST_NAME db "Invalid last name ref in address field", 0
+ERR_ADDR_SIZE_QUAL db "Size qualifier must be spesicied for address ref", 0
+ERR_INV_ADDR_ARGC db "Invalid amount of parameters for address field", 0
 
 ERR_INS_INV_1ST_PARAM db "Invalid 1st parameter of instruction", 0
 ERR_INS_INV_2ND_PARAM db "Invalid 2nd parameter of instruction", 0
