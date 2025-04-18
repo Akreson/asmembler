5aeb08f5dfb306c15b94fae71f5d75776e6c36ec
Akreson <gimules.but@gmail.com>
2025-04-18 17:23:05 +0200

    Bring global sym. back

--- parser.asm
+++ parser.asm
@@ -456,10 +456,10 @@ push_name_to_defined:
     jz _add_entry_pnt_def
     lea rsi, [NAME_SYM_REF_ARRAY]
     mov cl, [rdi+15]
+    mov [rax+15], cl
     and cl, SYM_REF_MASK_EXT
     cmp cl, SYM_REF_EXT_ENTRY
     jne _patch_pnt_def
-    mov [rax+15], cl
     mov ebx, [rbp-36]
     mov [ENTRY_SYM_OFFSET], ebx
     mov [ENTRY_SYM_ARR_PTR], rsi
